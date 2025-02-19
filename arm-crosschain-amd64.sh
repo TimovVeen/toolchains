c00b530f65c809c2543d87ef956632bd29e9b4a1
TimovVeen <timovanveen@hotmail.nl>
2025-02-19 20:18:53 +0100

    init

---
+++ arm-crosschain-amd64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c281276ff7a613d4eff0768105ce4a5c3a05d0b9ab27ddc999351b1c9e4e02b
+size 137307505
---
+++ arm-crosschain-arm64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aff344c75d71ce3b9ffc31bb31ea2402c46d6af5395cfa220c49d996dcef96f7
+size 127704522
